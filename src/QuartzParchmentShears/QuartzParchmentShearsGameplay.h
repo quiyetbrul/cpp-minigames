0dba904e09c979272c2dc9497ac105b164606365
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-21 11:04:53 -0700

    Update on headerfiles and some game mech

--- src/QuartzParchmentShears/QuartzParchmentShearsGameplay.h
+++
@@ -1,7 +0,0 @@
-#ifndef QUARTZPARCHMENTSHEARSGAMEPLAY_H
-#define QUARTZPARCHMENTSHEARSGAMEPLAY_H
-
-// this is the main file for the QuartzParchmentShears
-void QuartzParchmentShearsGameplay();
-
-#endif // QUARTZPARCHMENTSHEARSGAMEPLAY_H
