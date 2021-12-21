5c184e290fca4a10a98dc8e3f03ebd80f8b95d9b
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-20 17:32:59 -0700

    Create QuartzParchmentShearsGameplay.h

---
+++ src/QuartzParchmentShears/QuartzParchmentShearsGameplay.h
@@ -0,0 +1,7 @@
+#ifndef QUARTZPARCHMENTSHEARSGAMEPLAY_H
+#define QUARTZPARCHMENTSHEARSGAMEPLAY_H
+
+// this is the main file for the QuartzParchmentShears
+void QuartzParchmentShearsGameplay();
+
+#endif // QUARTZPARCHMENTSHEARSGAMEPLAY_H
