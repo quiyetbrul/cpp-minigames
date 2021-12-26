9b6b4b1cfd39ac6ad946f8aab0a847d6821ba595
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-26 01:44:59 -0700

    Welcome message

---
+++ src/GameWelcome.h
@@ -0,0 +1,12 @@
+#ifndef GAMEWELCOME_H
+#define GAMEWELCOME_H
+
+#include <string>
+#include <iostream>
+
+// Welcome
+// Precondition: gameName is a valid string, players is a boolean
+// Postcondition: displays a welcome message to the user
+void Welcome(std::string gameName, bool players);
+
+#endif // GAMEWELCOME_H
