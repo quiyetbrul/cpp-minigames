1ad8a56a51f8459d6b16f2106ae7c5f8c7875d5b
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2021-12-30 17:15:27 -0700

    Update GameWelcome.cpp

--- src/GameWelcome.cpp
+++ src/GameWelcome.cpp
@@ -4,4 +4,4 @@ void Welcome(std::string gameName, bool players) {
   std::cout << "Welcome to the game of " << gameName << "!" << std::endl;
   std::cout << "You are playing against "
             << (players ? "another player" : "a computer") << "." << std::endl;
-}
+} // -----------------------------------------------------------------------------
