dfca4b863f7d2d47bdcd2942e1c85cc60838512b
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2022-01-05 23:19:29 -0700

    Update GameHangman.cpp

--- src/GameHangman.cpp
+++ src/GameHangman.cpp
@@ -1,3 +1,15 @@
-#include "GameHangman.h"
+#include <chrono>
+#include <thread>
 
+#include "include/GameHangman.h"
 
+void Hangman::GameMechanics(){}
+
+void Hangman::GamePlay() {
+  system("clear");
+  std::cout << "Hangman has not been developed yet." << std::endl;
+  std::cout << "Please wait for the next update." << std::endl;
+  std::cout << "Returning to main menu..." << std::endl;
+  std::chrono::seconds dura(3);
+  std::this_thread::sleep_for(dura);
+}
